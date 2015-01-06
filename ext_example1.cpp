ad9618ac8cff194a2e01a2bf9d59d0ae5ec93939
Sara Golemon <pollita@php.net>
2015-01-05 19:01:36 -0800

    Add an example of declaring a constant

--- ext_example1.cpp
+++ ext_example1.cpp
@@ -17,11 +17,22 @@ void HHVM_FUNCTION(example1_life, VRefParam meaning) {
   meaning = 42;
 }
 
+const StaticString
+  s_EXAMPLE1_NOTHING("EXAMPLE1_NOTHING"),
+  s_EXAMPLE1_YEAR("EXAMPLE1_YEAR"),
+  s_EXAMPLE1_URL("EXAMPLE1_URL"),
+  s_URL("http://blog.golemon.com/2015/01/hhvm-extension-writing-part-i.html");
+
 class Example1Extension : public Extension {
  public:
   Example1Extension(): Extension("example1", "1.0") {}
 
   void moduleInit() override {
+    Native::registerConstant<KindOfNull>(s_EXAMPLE1_NOTHING.get());
+    Native::registerConstant<KindOfInt64>(s_EXAMPLE1_YEAR.get(), 2015);
+    Native::registerConstant<KindOfStaticString>(s_EXAMPLE1_URL.get(),
+                                                 s_URL.get());
+
     HHVM_FE(example1_greet);
     HHVM_FE(example1_is_odd);
     HHVM_FE(example1_life);
---
+++ tests/constants.php
@@ -0,0 +1,6 @@
+<?php
+
+var_dump(defined("EXAMPLE1_NOTHING"));
+var_dump(EXAMPLE1_NOTHING);
+var_dump(EXAMPLE1_YEAR);
+var_dump(EXAMPLE1_URL);
